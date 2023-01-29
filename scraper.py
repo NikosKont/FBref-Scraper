8bf8a916444b8834e95a88077ffb7f92fb581270
Nikos Kontogeorgis <51276659+NikosKont@users.noreply.github.com>
2023-01-29 04:57:43 +0200

    formatting corrections

--- scraper.py
+++ scraper.py
@@ -4,7 +4,17 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import re
 
-def getPlayerStatsTable(stat, compid):
+statsList = ['stats', 'keepers', 'keepersadv', 'shooting', 'passing', 'passing_types', 'gca', 'defense', 'possession', 'playingtime', 'misc']
+
+def getPlayerStats(stat: str, compid: int):
+    if stat not in statsList:
+        raise ValueError(f"stat must be one of {statsList}")
+
+    table = _getRawTable(stat, compid)
+    return _getDataframe(table)
+
+
+def _getRawTable(stat: str, compid: int):
     url = f"https://fbref.com/en/comps/{compid}/{stat}/"
     print(f"Getting data from {url}")
     res = requests.get(url)
@@ -14,7 +24,7 @@ def getPlayerStatsTable(stat, compid):
     return soup.find("div", {"id": f"div_stats_{stat}"})
 
 
-def getDataframe(table):
+def _getDataframe(table):
     df = pd.read_html(str(table))
     df = df[0]
 
@@ -25,21 +35,18 @@ def getDataframe(table):
     df.columns = [h[1] for h in df.columns]
 
     # only keep the part after space for 'Nation'
-    df['Nation'] = df['Nation'].apply(lambda x: str(x).split(' ')[-1])
+    df['Nation'] = df['Nation'].apply(lambda x: str(x).rsplit(' ', maxsplit=1)[-1])
 
     # delete rows with the column names
     df = df[df[df.columns[0]] != df.columns[0]]
     df.reset_index(drop=True, inplace=True)
 
     for col in df.columns:
-        df[col] = df[col].astype(float)
-        if df[col].apply(lambda x: x.is_integer()).all():
-            df[col] = df[col].astype(int)
+        try:
+            df[col] = df[col].astype(float)
+            if df[col].apply(lambda x: x.is_integer()).all():
+                df[col] = df[col].astype(int)
+        except ValueError:
+            pass
 
     return df
-
-
-def getPlayerStats(stat, compid):
-    table = getPlayerStatsTable(stat, compid)
-
-    return getDataframe(table)
