660b0f2dfbc14a5b4b2fd8fb83ec8f42cc2fb052
Nikos Kontogeorgis <51276659+NikosKont@users.noreply.github.com>
2023-05-31 14:11:18 +0300

    formatting corrections

--- scraper.py
+++ scraper.py
@@ -1,30 +1,45 @@
 
-import requests
-from bs4 import BeautifulSoup as bs
-import pandas as pd
 import re
-
-statsList = ['stats', 'keepers', 'keepersadv', 'shooting', 'passing', 'passing_types', 'gca', 'defense', 'possession', 'playingtime', 'misc']
-
-def getPlayerStats(stat: str, compid: int):
-    if stat not in statsList:
-        raise ValueError(f"stat must be one of {statsList}")
-
-    table = _getRawTable(stat, compid)
-    return _getDataframe(table)
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup as bs
 
 
-def _getRawTable(stat: str, compid: int):
-    url = f"https://fbref.com/en/comps/{compid}/{stat}/"
-    print(f"Getting data from {url}")
-    res = requests.get(url)
-    comm = re.compile("<!--|-->")
-    soup = bs(comm.sub("", res.text), 'lxml')
+stats_list = ('stats', 'keepers', 'keepersadv', 'shooting', 'passing','passing_types',
+              'gca', 'defense', 'possession', 'playingtime', 'misc')
 
-    return soup.find("div", {"id": f"div_stats_{stat}"})
+def available_stats():
+    """ Returns the available stats to scrape """
+    return stats_list
 
 
-def _getDataframe(table):
+def get_player_stats(stat: str, compid: int):
+    """ Get player stats from FBref.com
+    stat: string of the stat to get, must be one of the available stats
+    compid: int of the competition id, can be found in the url of the competition
+
+    returns: pandas dataframe of the stats
+    """
+    if stat not in stats_list:
+        raise ValueError(f'stat must be one of {stats_list}')
+
+    table = _get_raw_table(stat, compid)
+    df = _get_dataframe(table)
+    return df
+
+
+def _get_raw_table(stat: str, compid: int):
+    url = f'https://fbref.com/en/comps/{compid}/{stat}/'
+    print(f'Getting data from {url}...')
+    res = requests.get(url, timeout=10)
+    comm = re.compile('<!--|-->')
+    soup = bs(comm.sub('', res.text), 'lxml')
+    table = soup.find('div', {'id': f'div_stats_{stat}'})
+    print('Done.')
+    return table
+
+
+def _get_dataframe(table):
     df = pd.read_html(str(table))
     df = df[0]
 
@@ -41,12 +56,13 @@ def _getDataframe(table):
     df = df[df[df.columns[0]] != df.columns[0]]
     df.reset_index(drop=True, inplace=True)
 
+    # convert all numeric columns to float, then integer columns to int
     for col in df.columns:
         try:
             df[col] = df[col].astype(float)
             if df[col].apply(lambda x: x.is_integer()).all():
                 df[col] = df[col].astype(int)
-        except:
+        except ValueError:
             pass
 
     return df
